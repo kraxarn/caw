1e69193c34abdb8484a29a5e8c6619d6152dfdd1
kraxarn <kraxie@protonmail.com>
2025-05-31 14:22:23 +0200

    Stretch instead

--- src/lists/orderlist.cpp
+++ src/lists/orderlist.cpp
@@ -2,6 +2,7 @@
 #include "font.hpp"
 #include "models/ordermodel.hpp"
 
+#include <QHeaderView>
 #include <QShowEvent>
 #include <QStandardItemModel>
 #include <QTreeView>
@@ -15,19 +16,12 @@ OrderList::OrderList(QWidget *parent)
 
 	setFont(Font::monospace());
 	setRootIsDecorated(false);
+
+	header()->setSectionResizeMode(QHeaderView::Stretch);
+	setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 }
 
 void OrderList::showEvent(QShowEvent *event)
 {
 	QTreeView::showEvent(event);
-
-	for (auto i = 0; i < model()->columnCount(); i++)
-	{
-		resizeColumnToContents(i);
-
-		if (i < model()->columnCount() - 1)
-		{
-			setColumnWidth(i, static_cast<int>(static_cast<double>(columnWidth(i)) * 1.5));
-		}
-	}
 }
