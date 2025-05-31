f7e535b44e327ed48af4788d8e76ceef8c0389db
kraxarn <kraxie@protonmail.com>
2025-05-31 19:09:47 +0200

    Show hex as uppercase

--- src/format.hpp
+++ src/format.hpp
@@ -9,7 +9,8 @@ public:
 	static auto hex(T value) -> QString
 	{
 		return QStringLiteral("%1")
-			.arg(value, 2, 16, QChar::fromLatin1('0'));
+			.arg(value, 2, 16, QChar::fromLatin1('0'))
+			.toUpper();
 	}
 
 private:
