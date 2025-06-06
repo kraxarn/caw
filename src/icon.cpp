87f89d02dbc6562580abae05f9c37997e382d2d6
kraxarn <kraxie@protonmail.com>
2025-06-06 16:04:50 +0200

    Try SVG icons instead

--- src/icon.cpp
+++ src/icon.cpp
@@ -20,3 +20,37 @@ void Icon::set(QAction *action, const IconName icon)
 	action->setText(get(icon));
 	action->setFont(font());
 }
+
+auto Icon::get(const Value icon) -> QIcon
+{
+	const auto name = QStringLiteral(":/icons/solid/%1")
+		.arg(getIconName(icon));
+
+	return QIcon(name);
+}
+
+auto Icon::getIconName(const Value icon) -> QString
+{
+	const auto meta = QMetaEnum::fromType<Value>();
+	const auto key = meta.valueToKey(icon);
+
+	auto str = QString::fromUtf8(key);
+
+	for (auto i = str.length() - 1; i >= 0; i--)
+	{
+		if (!str.at(i).isUpper())
+		{
+			continue;
+		}
+
+		str.replace(i, 1, str.at(i).toLower());
+
+		if (i > 0)
+		{
+			str.insert(i, QChar::fromLatin1('-'));
+			i--;
+		}
+	}
+
+	return str;
+}
--- src/icon.hpp
+++ src/icon.hpp
@@ -6,10 +6,14 @@
 
 class Icon final
 {
+	Q_GADGET
+
 public:
 	[[nodiscard]]
+	[[deprecated]]
 	static auto get(IconName icon) -> QString;
 
+	[[deprecated]]
 	static void set(QAction *action, IconName icon);
 
 private:
@@ -17,4 +21,33 @@ private:
 
 	[[nodiscard]]
 	static auto font() -> QFont;
+
+public:
+	enum Value : quint8
+	{
+		Play,
+		ForwardStep,
+		RotateRight,
+		AnglesDown,
+		Circle,
+		Repeat,
+		Plus,
+		Minus,
+		Clone,
+		ChevronUp,
+		ChevronDown,
+		Link,
+		LinkSlash,
+		FolderOpen,
+		FloppyDisk,
+		Xmark,
+	};
+
+	Q_ENUM(Value)
+
+	static auto get(Value icon) -> QIcon;
+
+private:
+	[[nodiscard]]
+	static auto getIconName(Value icon) -> QString;
 };
