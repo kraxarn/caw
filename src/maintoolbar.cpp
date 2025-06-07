2d9397242b56a4baac8e95147ca38ccf03af4c07
kraxarn <kraxie@protonmail.com>
2025-06-07 14:02:18 +0200

    Slightly smaller buttons

--- src/maintoolbar.cpp
+++ src/maintoolbar.cpp
@@ -4,6 +4,7 @@
 MainToolBar::MainToolBar(QWidget *parent)
 	: QToolBar(parent)
 {
+	setIconSize(iconSize() * 0.75);
 	setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
 
 	addAction(Icon::get(this, IconName::FileCirclePlus), QStringLiteral("New"));
