cdfe524b0446183e3e45cd8e566593fdcce6406c
kraxarn <kraxie@protonmail.com>
2025-07-03 15:13:08 +0200

    Initial fx widget

--- src/windows/instrumenteditor.cpp
+++ src/windows/instrumenteditor.cpp
@@ -29,11 +29,11 @@ InstrumentEditor::InstrumentEditor(QWidget *parent)
 	layout->addWidget(envelope(), 3, 0);
 	layout->addWidget(lfo(), 3, 1);
 
-	auto *fx = new QGroupBox(QStringLiteral("FX"), this);
-	layout->addWidget(fx, 4, 0, 1, 2);
+	layout->addWidget(fx(), 4, 0, 1, 2);
 }
 
-auto InstrumentEditor::slider(const QString &text, const QWidget *parent) -> QSlider *
+auto InstrumentEditor::slider(const QString &text, const int row, const int column,
+	const int columnSpan, const QWidget *parent) -> QSlider *
 {
 	auto *layout = qobject_cast<QGridLayout *>(parent->layout());
 	if (layout == nullptr)
@@ -42,15 +42,37 @@ auto InstrumentEditor::slider(const QString &text, const QWidget *parent) -> QSl
 		return new QSlider();
 	}
 
-	const int row = layout->rowCount();
-
 	auto *label = new QLabel(text, this);
-	layout->addWidget(label, row, 0);
+	layout->addWidget(label, row, column);
 
 	auto *slider = new QSlider(Qt::Horizontal, this);
-	layout->addWidget(slider, row, 1);
+	layout->addWidget(slider, row, column + 1, 1, columnSpan);
 
 	return slider;
+}
+
+auto InstrumentEditor::slider(const QString &text, const int row, const int column, const QWidget *parent) -> QSlider *
+{
+	return slider(text, row, column, 1, parent);
+}
+
+auto InstrumentEditor::slider(const QString &text, const int rowSpan, const QWidget *parent) -> QSlider *
+{
+	const auto *layout = qobject_cast<QGridLayout *>(parent->layout());
+	if (layout == nullptr)
+	{
+		qFatal() << "no layout:" << parent->objectName();
+		return new QSlider();
+	}
+
+	const int row = layout->rowCount();
+
+	return slider(text, row, 0, rowSpan, parent);
+}
+
+auto InstrumentEditor::slider(const QString &text, const QWidget *parent) -> QSlider *
+{
+	return slider(text, 1, parent);
 }
 
 auto InstrumentEditor::preset() -> QWidget *
@@ -202,6 +224,49 @@ auto InstrumentEditor::lfo() -> QWidget *
 
 	auto *freqSlider = slider(QStringLiteral("FREQ"), group);
 	freqSlider->setRange(0, 16);
+
+	return group;
+}
+
+auto InstrumentEditor::fx() -> QWidget *
+{
+	auto *group = new QGroupBox(QStringLiteral("FX"), this);
+	auto *layout = new QGridLayout(group);
+	layout->setAlignment(Qt::AlignTop);
+
+	auto *filterLabel = new QLabel(QStringLiteral("Filter"), this);
+	layout->addWidget(filterLabel, 0, 0);
+
+	auto *filterComboBox = new QComboBox(this);
+	filterComboBox->addItems({
+		QStringLiteral("None"),
+		QStringLiteral("Low pass"),
+		QStringLiteral("High pass"),
+		QStringLiteral("Band pass"),
+		QStringLiteral("Notch"),
+	});
+	layout->addWidget(filterComboBox, 0, 1, 1, 3);
+
+	auto *freqSlider = slider(QStringLiteral("Filter frequency"), 3, group);
+	freqSlider->setRange(0, 11025);
+
+	auto *resSlider = slider(QStringLiteral("Filter resonance"), 3, group);
+	resSlider->setRange(0, 255);
+
+	auto *delaySlider = slider(QStringLiteral("Delay"), group);
+	delaySlider->setRange(0, 248);
+
+	auto *delayTimeSlider = slider(QStringLiteral("Time"), 3, 2, group);
+	delayTimeSlider->setRange(0, 16);
+
+	auto *panSlider = slider(QStringLiteral("Pan"), group);
+	panSlider->setRange(0, 255);
+
+	auto *panFreqSlider = slider(QStringLiteral("Frequency"), 4, 2, group);
+	panFreqSlider->setRange(0, 16);
+
+	auto *noiseSlider = slider(QStringLiteral("Noise volume"), 3, group);
+	noiseSlider->setRange(0, 255);
 
 	return group;
 }
--- src/windows/instrumenteditor.hpp
+++ src/windows/instrumenteditor.hpp
@@ -11,10 +11,23 @@ public:
 	explicit InstrumentEditor(QWidget *parent);
 
 private:
+	[[nodiscard]]
 	auto preset() -> QWidget *;
+	[[nodiscard]]
 	auto osc(quint8 idx) -> QWidget *;
+	[[nodiscard]]
 	auto envelope() -> QWidget *;
+	[[nodiscard]]
 	auto lfo() -> QWidget *;
+	[[nodiscard]]
+	auto fx() -> QWidget *;
 
+	[[nodiscard]]
 	auto slider(const QString &text, const QWidget *parent) -> QSlider *;
+	[[nodiscard]]
+	auto slider(const QString &text, int rowSpan, const QWidget *parent) -> QSlider *;
+	[[nodiscard]]
+	auto slider(const QString &text, int row, int column, const QWidget *parent) -> QSlider *;
+	[[nodiscard]]
+	auto slider(const QString &text, int row, int column, int columnSpan, const QWidget *parent) -> QSlider *;
 };
