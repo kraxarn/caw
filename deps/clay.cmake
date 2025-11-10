af88e87c63eade42c1566ac550ba4475ebc93f8a
kraxarn <kraxie@protonmail.com>
2025-11-10 18:01:16 +0100

    Use custom fork for the time being

--- deps/clay.cmake
+++ deps/clay.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
 
 FetchContent_Declare(clay
-	GIT_REPOSITORY https://github.com/nicbarker/clay.git
-	GIT_TAG v0.14
+	GIT_REPOSITORY https://github.com/kraxarn/clay.git
+	GIT_TAG 4d702367116ec734bbda52ad2d09847519db85a4
 	SOURCE_SUBDIR ignore
 )
 
