026026f5d919d44ef4fce74f5218a2ba8e761abf
kraxarn <kraxie@protonmail.com>
2025-11-13 17:35:43 +0100

    Initial icon generation for embedding

--- deps/mdi.cmake
+++ deps/mdi.cmake
@@ -6,3 +6,20 @@ FetchContent_Declare(mdi
 )
 message(STATUS "Downloading icons")
 FetchContent_MakeAvailable(mdi)
+
+set(MDI_DIR "${mdi_SOURCE_DIR}/svg")
+set(MDI_ICONS
+	"checkbox-blank-outline.svg"
+	"checkbox-marked.svg"
+)
+
+foreach (ICON IN LISTS MDI_ICONS)
+	string(LENGTH "${ICON}" ICON_LENGTH)
+	math(EXPR ICON_LENGTH "${ICON_LENGTH} - 4")
+	string(SUBSTRING "${ICON}" 0 "${ICON_LENGTH}" ICON_DEFINE)
+	string(REPLACE "-" "_" ICON_DEFINE "${ICON_DEFINE}")
+	string(TOUPPER "${ICON_DEFINE}" ICON_DEFINE)
+	target_compile_definitions(${PROJECT_NAME} PRIVATE
+		"IC_${ICON_DEFINE}=\"${mdi_SOURCE_DIR}/svg/${ICON}\""
+	)
+endforeach ()
