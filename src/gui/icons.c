9d78408ee4fd03fe88b976448bde76c830033249
kraxarn <kraxie@protonmail.com>
2025-11-14 13:21:36 +0100

    Manually set colour of surface

--- src/gui/icons.c
+++ src/gui/icons.c
@@ -2,16 +2,42 @@
 #include "caw/logcategory.h"
 
 #include <SDL3/SDL_log.h>
+#include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_properties.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_stdinc.h>
 #include <SDL3/SDL_surface.h>
 #include <SDL3_image/SDL_image.h>
+
+#include "caw/gui/apptheme.h"
 
 static SDL_PropertiesID props = 0;
 
 void cleanup([[maybe_unused]] void *userdata, void *value)
 {
 	SDL_DestroyTexture(value);
+}
+
+// Is there a better way to do this?
+void set_color(SDL_Surface *surface, const SDL_Color color)
+{
+	for (auto x = 0; x < surface->w; x++)
+	{
+		for (auto y = 0; y < surface->h; y++)
+		{
+			Uint8 r;
+			Uint8 g;
+			Uint8 b;
+			Uint8 a;
+
+			if (!SDL_ReadSurfacePixel(surface, x, y, &r, &g, &b, &a) || a == 0)
+			{
+				continue;
+			}
+
+			SDL_WriteSurfacePixel(surface, x, y, color.r, color.g, color.b, color.a);
+		}
+	}
 }
 
 SDL_Texture *icon(SDL_Renderer *renderer, const char *name)
@@ -49,6 +75,8 @@ SDL_Texture *icon(SDL_Renderer *renderer, const char *name)
 		SDL_LogError(LOG_CATEGORY_ICONS, "Failed to parse icon: %s", SDL_GetError());
 		return nullptr;
 	}
+
+	set_color(surface, app_color_sdl(COLOR_FOREGROUND));
 
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 	if (texture == nullptr)
