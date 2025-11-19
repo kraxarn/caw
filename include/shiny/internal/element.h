ae69f19ecb602b7c18f39866115d16b2bb003bf0
kraxarn <kraxie@protonmail.com>
2025-11-19 17:07:57 +0100

    Remove stuff already provided by shiny

--- include/shiny/internal/element.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "clay.h"
-
-void shiny_element_open(const char *element_id);
-
-void shiny_element_configure(const Clay_ElementDeclaration *declaration);
-
-void shiny_element_close();
--- src/widgets/readme.md
+++
@@ -1,1 +0,0 @@
-this will later be moved to its own ui library once it's a bit more complete
