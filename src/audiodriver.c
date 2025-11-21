5a97af330a17d9e397b4cd689e49ff90e5d2e8db
kraxarn <kraxie@protonmail.com>
2025-11-21 13:35:07 +0100

    Add audio driver display name cache

--- src/audiodriver.c
+++ src/audiodriver.c
@@ -1,103 +1,50 @@
 #include "caw/audiodriver.h"
+#include "caw/logcategory.h"
 
-#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_properties.h>
+
+SDL_PropertiesID audio_driver_props = 0;
+
+bool update_audio_driver_display_name_cache()
+{
+	audio_driver_props = SDL_CreateProperties();
+	if (audio_driver_props == 0)
+	{
+		SDL_LogWarn(LOG_CATEGORY_GUI, "Render driver cache error: %s", SDL_GetError());
+		return false;
+	}
+
+	SDL_SetStringProperty(audio_driver_props, "pipewire", "PipeWire");
+	SDL_SetStringProperty(audio_driver_props, "pulseaudio", "PulseAudio");
+	SDL_SetStringProperty(audio_driver_props, "alsa", "ALSA");
+	SDL_SetStringProperty(audio_driver_props, "netbsd", "NetBSD");
+	SDL_SetStringProperty(audio_driver_props, "wasapi", "WASAPI");
+	SDL_SetStringProperty(audio_driver_props, "directsound", "DirectSound");
+	SDL_SetStringProperty(audio_driver_props, "haiku", "BSoundPlayer");
+	SDL_SetStringProperty(audio_driver_props, "coreaudio", "CoreAudio");
+	SDL_SetStringProperty(audio_driver_props, "openslES", "OpenSL ES");
+	SDL_SetStringProperty(audio_driver_props, "ps2", "PS2");
+	SDL_SetStringProperty(audio_driver_props, "psp", "PSP");
+	SDL_SetStringProperty(audio_driver_props, "vita", "Vita");
+	SDL_SetStringProperty(audio_driver_props, "n3ds", "N3DS");
+	SDL_SetStringProperty(audio_driver_props, "emscripten", "Web Audio API");
+	SDL_SetStringProperty(audio_driver_props, "jack", "JACK");
+	SDL_SetStringProperty(audio_driver_props, "dsp", "OSS");
+	SDL_SetStringProperty(audio_driver_props, "qsa", "QNX QSA");
+	SDL_SetStringProperty(audio_driver_props, "disk", "Disk");
+	SDL_SetStringProperty(audio_driver_props, "dummy", "No audio");
+
+	return true;
+}
 
 const char *audio_driver_display_name(const char *name)
 {
-	if (SDL_strcmp(name, "pipewire") == 0)
+	if (audio_driver_props == 0 && !update_audio_driver_display_name_cache())
 	{
-		return "PipeWire";
+		return name;
 	}
 
-	if (SDL_strcmp(name, "pulseaudio") == 0)
-	{
-		return "PulseAudio";
-	}
-
-	if (SDL_strcmp(name, "alsa") == 0)
-	{
-		return "ALSA";
-	}
-
-	if (SDL_strcmp(name, "netbsd") == 0)
-	{
-		return "NetBSD";
-	}
-
-	if (SDL_strcmp(name, "wasapi") == 0)
-	{
-		return "WASAPI";
-	}
-
-	if (SDL_strcmp(name, "directsound") == 0)
-	{
-		return "DirectSound";
-	}
-
-	if (SDL_strcmp(name, "haiku") == 0)
-	{
-		return "BSoundPlayer";
-	}
-
-	if (SDL_strcmp(name, "coreaudio") == 0)
-	{
-		return "CoreAudio";
-	}
-
-	if (SDL_strcmp(name, "openslES") == 0)
-	{
-		return "OpenSL ES";
-	}
-
-	if (SDL_strcmp(name, "ps2") == 0)
-	{
-		return "PS2";
-	}
-
-	if (SDL_strcmp(name, "psp") == 0)
-	{
-		return "PSP";
-	}
-
-	if (SDL_strcmp(name, "vita") == 0)
-	{
-		return "Vita";
-	}
-
-	if (SDL_strcmp(name, "n3ds") == 0)
-	{
-		return "N3DS";
-	}
-
-	if (SDL_strcmp(name, "emscripten") == 0)
-	{
-		return "Web Audio API";
-	}
-
-	if (SDL_strcmp(name, "jack") == 0)
-	{
-		return "JACK";
-	}
-
-	if (SDL_strcmp(name, "dsp") == 0)
-	{
-		return "OSS";
-	}
-
-	if (SDL_strcmp(name, "qsa") == 0)
-	{
-		return "QNX QSA";
-	}
-
-	if (SDL_strcmp(name, "disk") == 0)
-	{
-		return "Disk";
-	}
-
-	if (SDL_strcmp(name, "dummy") == 0)
-	{
-		return "No audio";
-	}
-
-	return name;
+	return SDL_GetStringProperty(audio_driver_props, name, name);
 }
