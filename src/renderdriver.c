8b1ca792d69d23e2540a607f66847f1513f991c3
kraxarn <kraxie@protonmail.com>
2025-11-21 13:36:28 +0100

    Add render driver display name cache

--- src/renderdriver.c
+++ src/renderdriver.c
@@ -1,68 +1,42 @@
 #include "caw/renderdriver.h"
+#include "caw/logcategory.h"
 
-#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_properties.h>
+
+SDL_PropertiesID render_driver_props = 0;
+
+bool update_render_driver_display_name_cache()
+{
+	render_driver_props = SDL_CreateProperties();
+	if (render_driver_props == 0)
+	{
+		SDL_LogWarn(LOG_CATEGORY_GUI, "Render driver cache error: %s", SDL_GetError());
+		return false;
+	}
+
+	SDL_SetStringProperty(render_driver_props, "direct3d11", "DirectX 11");
+	SDL_SetStringProperty(render_driver_props, "direct3d12", "DirectX 12");
+	SDL_SetStringProperty(render_driver_props, "direct3d", "DirectX 9");
+	SDL_SetStringProperty(render_driver_props, "metal", "Metal");
+	SDL_SetStringProperty(render_driver_props, "opengl", "OpenGL");
+	SDL_SetStringProperty(render_driver_props, "opengles2", "OpenGL ES 2.0");
+	SDL_SetStringProperty(render_driver_props, "PS2 gsKit", "gsKit");
+	SDL_SetStringProperty(render_driver_props, "VITA gxm", "GXM");
+	SDL_SetStringProperty(render_driver_props, "vulkan", "Vulkan");
+	SDL_SetStringProperty(render_driver_props, "gpu", "GPU API (VK/DX12/MTL)");
+	SDL_SetStringProperty(render_driver_props, "software", "Software");
+
+	return true;
+}
 
 const char *render_driver_display_name(const char *name)
 {
-	if (SDL_strcmp(name, "direct3d11") == 0)
+	if (render_driver_props == 0 && !update_render_driver_display_name_cache())
 	{
-		return "DirectX 11";
+		return name;
 	}
 
-	if (SDL_strcmp(name, "direct3d12") == 0)
-	{
-		return "DirectX 12";
-	}
-
-	if (SDL_strcmp(name, "direct3d") == 0)
-	{
-		return "DirectX 9";
-	}
-
-	if (SDL_strcmp(name, "metal") == 0)
-	{
-		return "Metal";
-	}
-
-	if (SDL_strcmp(name, "opengl") == 0)
-	{
-		return "OpenGL";
-	}
-
-	if (SDL_strcmp(name, "opengles2") == 0)
-	{
-		return "OpenGL ES 2.0";
-	}
-
-	if (SDL_strcmp(name, "PS2 gsKit") == 0)
-	{
-		return "gsKit";
-	}
-
-	if (SDL_strcmp(name, "PSP") == 0)
-	{
-		return "PSP";
-	}
-
-	if (SDL_strcmp(name, "VITA gxm") == 0)
-	{
-		return "GXM";
-	}
-
-	if (SDL_strcmp(name, "vulkan") == 0)
-	{
-		return "Vulkan";
-	}
-
-	if (SDL_strcmp(name, "gpu") == 0)
-	{
-		return "GPU API (VK/DX12/MTL)";
-	}
-
-	if (SDL_strcmp(name, "software") == 0)
-	{
-		return "Software";
-	}
-
-	return name;
+	return SDL_GetStringProperty(render_driver_props, name, name);
 }
