a1b6980180dfe1d6b717d652e525b84d8db80b2d
kraxarn <kraxie@protonmail.com>
2025-12-05 14:48:06 +0100

    Use shiny window

--- src/gui/windowsettings.c
+++ src/gui/windowsettings.c
@@ -10,13 +10,14 @@
 #include "shiny/comboboxoption.h"
 #include "shiny/init.h"
 #include "shiny/spacer.h"
-#include "shiny/theme.h"
 #include "shiny/themekey.h"
+#include "shiny/window.h"
 #include "shiny/internal/color.h"
 
 #include "clay.h"
 
 #include <SDL3/SDL_audio.h>
+#include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
 
 typedef const char *(*cb_item_callback_t)(int index);
@@ -28,14 +29,6 @@ typedef struct cb_settings_t
 	int size;
 	cb_select_callback_t callback;
 } cb_settings_t;
-
-Clay_TextElementConfig title_text_config()
-{
-	return (Clay_TextElementConfig){
-		.fontSize = FONT_SIZE_TITLE,
-		.textColor = shiny_clay_theme_color(SHINY_COLOR_FOREGROUND),
-	};
-}
 
 Clay_TextElementConfig body_text_config()
 {
@@ -91,60 +84,6 @@ void set_audio_driver(app_state_t *state, const int index)
 	settings_flush(state->settings);
 }
 
-void on_window_title_hover(const Clay_ElementId element_id,
-	const Clay_PointerData pointer_data, const intptr_t user_data)
-{
-	const auto state = (gui_window_state_t *) user_data;
-
-	if (pointer_data.state == CLAY_POINTER_DATA_PRESSED)
-	{
-		if (state->offset.x == 0 || state->offset.y == 0)
-		{
-			const auto data = Clay_GetElementData(element_id);
-			state->offset.x = pointer_data.position.x - data.boundingBox.x;
-			state->offset.y = pointer_data.position.y - data.boundingBox.y;
-			state->attach_point = CLAY_ATTACH_POINT_LEFT_TOP;
-		}
-
-		state->position.x = pointer_data.position.x - (state->offset.x);
-		state->position.y = pointer_data.position.y - (state->offset.y);
-	}
-
-	if (pointer_data.state == CLAY_POINTER_DATA_RELEASED_THIS_FRAME)
-	{
-		state->offset = (Clay_Vector2){};
-	}
-}
-
-void window_title(const app_state_t *state, const Clay_String text)
-{
-	const Clay_ElementDeclaration element = {
-		.layout = (Clay_LayoutConfig){
-			.sizing = (Clay_Sizing){
-				.width = CLAY_SIZING_GROW(0),
-			},
-			.padding = (Clay_Padding){
-				.left = shiny_theme_padding(SHINY_PADDING_WINDOW_HEADER_X),
-				.right = shiny_theme_padding(SHINY_PADDING_WINDOW_HEADER_X),
-				.top = shiny_theme_padding(SHINY_PADDING_WINDOW_HEADER_Y),
-				.bottom = shiny_theme_padding(SHINY_PADDING_WINDOW_HEADER_Y),
-			},
-		},
-		.backgroundColor = shiny_clay_theme_color(SHINY_COLOR_WINDOW_HEADER),
-		.cornerRadius = (Clay_CornerRadius){
-			.topLeft = shiny_theme_corner_radius(SHINY_CORNER_RADIUS_WINDOW),
-			.topRight = shiny_theme_corner_radius(SHINY_CORNER_RADIUS_WINDOW),
-		},
-	};
-
-	CLAY_AUTO_ID(element)
-	{
-		Clay_OnHover(on_window_title_hover, (intptr_t) &state->gui.windows.settings);
-
-		CLAY_TEXT(text, CLAY_TEXT_CONFIG(title_text_config()));
-	}
-}
-
 void combobox(app_state_t *state, const char *element_id, const cb_settings_t settings)
 {
 	Clay_Context *context = shiny_state_clay_context(state->shiny);
@@ -167,23 +106,6 @@ void window_content(app_state_t *state)
 {
 	Clay_Context *context = shiny_state_clay_context(state->shiny);
 
-	const Clay_ElementDeclaration wrapper = {
-		.layout = (Clay_LayoutConfig){
-			.layoutDirection = CLAY_TOP_TO_BOTTOM,
-			.childGap = shiny_theme_gap(SHINY_GAP_WINDOW_ITEM),
-			.sizing = (Clay_Sizing){
-				.width = CLAY_SIZING_GROW(0),
-				.height = CLAY_SIZING_GROW(0),
-			},
-			.padding = CLAY_PADDING_ALL(shiny_theme_padding(SHINY_PADDING_WINDOW_CONTENT)),
-		},
-		.backgroundColor = shiny_clay_theme_color(SHINY_COLOR_WINDOW_BACKGROUND),
-		.cornerRadius = (Clay_CornerRadius){
-			.bottomLeft = shiny_theme_corner_radius(SHINY_CORNER_RADIUS_WINDOW),
-			.bottomRight = shiny_theme_corner_radius(SHINY_CORNER_RADIUS_WINDOW),
-		},
-	};
-
 	const Clay_ElementDeclaration content = {
 		.layout = (Clay_LayoutConfig){
 			.layoutDirection = CLAY_LEFT_TO_RIGHT,
@@ -196,70 +118,48 @@ void window_content(app_state_t *state)
 		},
 	};
 
-	CLAY_AUTO_ID(wrapper)
+	CLAY_AUTO_ID(content)
 	{
-		CLAY_AUTO_ID(content)
-		{
-			CLAY_TEXT(CLAY_STRING("Renderer"), CLAY_TEXT_CONFIG(body_text_config()));
-			shiny_h_spacer(context);
-			combobox(state, "Renderer", (cb_settings_t){
-				.value = state->gui.settings.renderer == nullptr
-					? render_driver(0)
-					: render_driver_display_name(state->gui.settings.renderer),
-				.items = render_driver,
-				.size = SDL_GetNumRenderDrivers() + 1,
-				.callback = set_render_driver,
-			});
-		}
-		CLAY_AUTO_ID(content)
-		{
-			CLAY_TEXT(CLAY_STRING("Audio driver"), CLAY_TEXT_CONFIG(body_text_config()));
-			shiny_h_spacer(context);
-			combobox(state, "AudioDriver", (cb_settings_t){
-				.value = state->gui.settings.audio_driver == nullptr
-					? render_driver(0)
-					: audio_driver_display_name(state->gui.settings.audio_driver),
-				.items = audio_driver,
-				.size = SDL_GetNumAudioDrivers() + 1,
-				.callback = set_audio_driver,
-			});
-		}
+		CLAY_TEXT(CLAY_STRING("Renderer"), CLAY_TEXT_CONFIG(body_text_config()));
+		shiny_h_spacer(context);
+		combobox(state, "Renderer", (cb_settings_t){
+			.value = state->gui.settings.renderer == nullptr
+				? render_driver(0)
+				: render_driver_display_name(state->gui.settings.renderer),
+			.items = render_driver,
+			.size = SDL_GetNumRenderDrivers() + 1,
+			.callback = set_render_driver,
+		});
+	}
+	CLAY_AUTO_ID(content)
+	{
+		CLAY_TEXT(CLAY_STRING("Audio driver"), CLAY_TEXT_CONFIG(body_text_config()));
+		shiny_h_spacer(context);
+		combobox(state, "AudioDriver", (cb_settings_t){
+			.value = state->gui.settings.audio_driver == nullptr
+				? render_driver(0)
+				: audio_driver_display_name(state->gui.settings.audio_driver),
+			.items = audio_driver,
+			.size = SDL_GetNumAudioDrivers() + 1,
+			.callback = set_audio_driver,
+		});
 	}
 }
 
 void settings_window(app_state_t *state)
 {
+	Clay_Context *context = shiny_state_clay_context(state->shiny);
+
 	constexpr auto width = 340.F;
 	constexpr auto height = 300.F;
-
-	const auto window = &state->gui.windows.settings;
-	if (window->position.x == 0 || window->position.y == 0)
-	{
-		window->position.x = -width / 2;
-		window->position.y = -height / 2;
-		window->attach_point = CLAY_ATTACH_POINT_CENTER_CENTER;
-	}
-
-	const Clay_ElementDeclaration element = {
-		.floating = (Clay_FloatingElementConfig){
-			.attachTo = CLAY_ATTACH_TO_ROOT,
-			.attachPoints = (Clay_FloatingAttachPoints){
-				.parent = window->attach_point,
-			},
-			.offset = window->position,
-		},
-		.layout = (Clay_LayoutConfig){
-			.layoutDirection = CLAY_TOP_TO_BOTTOM,
-			.sizing = (Clay_Sizing){
-				.width = CLAY_SIZING_FIXED(width),
-				.height = CLAY_SIZING_FIXED(height),
-			},
-		},
+	const SDL_FRect size = {
+		.w = width,
+		.h = height,
 	};
 
-	CLAY(CLAY_ID("SettingsWindow"), element)
+	shiny_window_begin(context, "SettingsWindow", size, "Settings", FONT_SIZE_TITLE);
 	{
-		window_title(state, CLAY_STRING("Settings"));
 		window_content(state);
 	}
+	shiny_window_end();
 }
